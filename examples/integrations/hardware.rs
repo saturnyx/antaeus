23fd1ac867d78ec2f3a2f22b50a9399668f0ccdd
Harshal <saturnyx@disroot.org>
2026-06-16 21:55:47 +0800

    feat: added integrations example

---
+++ examples/integrations/hardware.rs
@@ -0,0 +1,27 @@
+use antaeus::*;
+use vexide::prelude::*;
+
+pub struct Robot {
+    pub main_con: Controller,
+    pub dt:       peripherals::drivetrain::Differential,
+}
+
+impl Robot {
+    pub fn default_config(peripherals: Peripherals) -> Robot {
+        Robot {
+            main_con: peripherals.primary_controller,
+            dt:       peripherals::drivetrain::Differential::new(
+                [
+                    Motor::new(peripherals.port_1, Gearset::Blue, Direction::Forward),
+                    Motor::new(peripherals.port_2, Gearset::Blue, Direction::Forward),
+                    Motor::new(peripherals.port_3, Gearset::Blue, Direction::Forward),
+                ],
+                [
+                    Motor::new(peripherals.port_4, Gearset::Blue, Direction::Reverse),
+                    Motor::new(peripherals.port_5, Gearset::Blue, Direction::Reverse),
+                    Motor::new(peripherals.port_6, Gearset::Blue, Direction::Reverse),
+                ],
+            ),
+        }
+    }
+}
